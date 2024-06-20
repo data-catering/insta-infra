88116640084d2f6dd6585afcda430c1165123847
Flook Peter <pflooky@hotmail.com>
2024-06-20 15:08:38 +0800

    Allow external data folders to be referenced via env vars

--- data/cassandra/init.sh
+++ data/cassandra/init.sh
@@ -6,7 +6,7 @@ total=0
 for f in $(ls /tmp/data/*.cql);
 do
   total=$((total + 1))
-  cqlsh -u "${CASSANDRA_USER:-cassandra}" -p "${CASSANDRA_PASSWORD:-cassandra}" -f "${f}" cassandra 9042
+  cqlsh -u "${CASSANDRA_USER:-cassandra}" -p "${CASSANDRA_PASSWORD:-cassandra}" -f "${f}" cassandra-server 9042
   if [[ $? -eq 0 ]];
   then
     count=$((count + 1))
