a0dae717529f1b15139c0ee03611875451dccd03
Flook Peter <pflooky@hotmail.com>
2024-07-12 15:19:57 +0800

    Fix kafka init by using port 29092 when creating topics

--- data/kafka/init.sh
+++ data/kafka/init.sh
@@ -11,7 +11,7 @@ fi
 
 echo "Creating kafka topics"
 for t in $(echo "$KAFKA_TOPICS" | sed s/,/\\n/g); do
-  kafka-topics --create --if-not-exists --topic "${t}" --bootstrap-server ${host}:9092 --replication-factor 1 --partitions 1
+  kafka-topics --create --if-not-exists --topic "${t}" --bootstrap-server ${host}:29092 --replication-factor 1 --partitions 1
 done
 
 echo "Successfully created the following topics:"
